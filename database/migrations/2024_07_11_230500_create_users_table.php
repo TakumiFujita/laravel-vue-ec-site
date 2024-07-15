3c9d78c4699a1f232b5af49115d66994231b5a0e
TakumiFujita <46127387+TakumiFujita@users.noreply.github.com>
2024-07-15 10:03:49 +0900

    make migration files

--- database/migrations/2024_07_11_230500_create_users_table.php
+++ database/migrations/2024_07_11_230500_create_users_table.php
@@ -17,8 +17,13 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('address');
+            $table->string('phone_number');
+            $table->unsignedBigInteger('role_id')->nullable();
             $table->rememberToken();
             $table->timestamps();
+
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('set null');
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
